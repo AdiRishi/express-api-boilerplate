2c0d7ca7f40dff50e31efc9a4817440aeb541a40
Adishwar Rishi <adiswa123@gmail.com>
2021-11-23 01:02:04 +1100

    Add helment js for security

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import config from './config'; // import config first to set NODE_ENV
 import express from 'express';
+import helmet from 'helmet';
 import Logger, { initApp } from './utils/logging';
 import ApiModuleWrapper from './api-routes';
 
@@ -7,6 +8,7 @@ const app = express();
 
 ApiModuleWrapper(app);
 initApp(app);
+app.use(helmet());
 
 app.listen(config.port, config.host, () => {
   Logger.info(`express-js-template is listening at http://${config.host}:${config.port}`);

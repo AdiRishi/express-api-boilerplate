f50f21ebb8dae19cf7868b4eac7513feeb39e9bc
Adishwar Rishi <adiswa123@gmail.com>
2021-11-25 13:58:05 +1100

    Add a graceful http terminator

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
+import process from 'process';
 import config from './config'; // import config first to set NODE_ENV
 import express from 'express';
 import helmet from 'helmet';
+import { createHttpTerminator } from 'http-terminator';
 import Logger, { initApp } from './utils/logging';
 import ApiModuleWrapper from './api-routes';
 
@@ -10,6 +12,14 @@ ApiModuleWrapper(app);
 initApp(app);
 app.use(helmet());
 
-app.listen(config.port, config.host, () => {
+const server = app.listen(config.port, config.host, () => {
   Logger.info(`express-js-template is listening at http://${config.host}:${config.port}`);
+});
+
+const httpTerminator = createHttpTerminator({
+  server,
+});
+
+process.on('SIGTERM', async () => {
+  await httpTerminator.terminate();
 });

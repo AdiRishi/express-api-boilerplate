2a6fcc486debc2f11d17d8915d43ff96daa6c373
Adishwar Rishi <adiswa123@gmail.com>
2021-12-03 17:23:33 +1100

    Setup cors

--- app.ts
+++ app.ts
@@ -2,15 +2,18 @@ import process from 'process';
 import config from './config'; // import config first to set NODE_ENV
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 import { createHttpTerminator } from 'http-terminator';
 import Logger, { initApp } from './utils/logging';
 import ApiModuleWrapper from './api-routes';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 ApiModuleWrapper(app);
 initApp(app);
-app.use(helmet());
 
 const server = app.listen(config.port, config.host, () => {
   Logger.info(`express-js-template is listening at http://${config.host}:${config.port}`);
